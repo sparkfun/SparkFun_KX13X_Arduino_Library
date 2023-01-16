c2d8a88b9bf912d090b8821202ce91c00f238a49
Paul <pc235603@gmail.com>
2023-01-16 13:05:48 +0000

    Correct enableDataEngine - allow the drdye bit to be cleared as well as set

--- src/SparkFun_Qwiic_KX13X.cpp
+++ src/SparkFun_Qwiic_KX13X.cpp
@@ -213,7 +213,10 @@ bool QwDevKX13X::enableDataEngine(bool enable)
   if (retVal != 0)
     return false;
 
-  tempVal = tempVal | (enable << 5);
+  sfe_kx13x_cntl1_bitfield_t cntl1;
+  cntl1.all = tempVal;
+  cntl1.bits.drdye =  enable; // This is a long winded but definitive way of setting/clearing the data ready engine bit
+  tempVal = cntl1.all;
 
   retVal = writeRegisterByte(SFE_KX13X_CNTL1, tempVal);
 
